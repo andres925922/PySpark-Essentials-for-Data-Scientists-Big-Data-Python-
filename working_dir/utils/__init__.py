a11b5f7f7eac48833f901504c7840c799d80d151
Andres Convertini <andres.convertini91@gmail.com>
2023-07-21 12:35:21 -0300

    Section 2

---
+++ working_dir/utils/__init__.py
@@ -0,0 +1,15 @@
+from typing import Any
+import pyspark
+from pyspark.sql import SparkSession
+
+class Session:
+    
+    def __init__(self, app_name: str) -> None:
+        self._session =  SparkSession  \
+                            .builder    \
+                            .appName(app_name)    \
+                            .getOrCreate()
+    
+    @property
+    def session(self):
+        return self._session
